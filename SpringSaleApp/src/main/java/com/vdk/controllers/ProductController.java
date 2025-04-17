4e14504d4dffac83a66d77c1cb4cc5067bfd05ca
Shriek-356 <khangvskiss@gmail.com>
2025-04-18 02:11:40 +0700

    update pratice 18/4

--- SpringSaleApp/src/main/java/com/vdk/controllers/ProductController.java
+++ SpringSaleApp/src/main/java/com/vdk/controllers/ProductController.java
@@ -9,8 +9,10 @@ import com.vdk.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,11 +35,21 @@ public class ProductController {
     }
 
     @PostMapping("/add")
-    public String create(@ModelAttribute (value="product") Product p){//product ban dau la rong, sau khi xu ly form xong se gui lai product cho cai nay
-        
-        this.productService.addOrUpdate(p);
-        
+    public String create(@ModelAttribute (value="product") Product p){//product ban dau la rong, sau khi xu ly form xong se gui lai product cho cai nay       
+        this.productService.addOrUpdate(p);       
         return "redirect:/";
     }
     
+    @GetMapping("/products/{productId}")
+    public String productView(Model model, @PathVariable (value="productId") int id ){//Lay product co id tuong ung
+        model.addAttribute("product",this.productService.getProductById(id));//Sau khi lay product se tra ve form them/cap nhat san pham voi product tuong ung
+        return "products";
+    }
+    
+    
+    @DeleteMapping("/delete/{productId}")
+    public String delete(Model model, @PathVariable (value="productId") int id){
+        this.productService.deleteProduct(id);//Xoa san pham
+        return "redirect:/";
+    }
 }
